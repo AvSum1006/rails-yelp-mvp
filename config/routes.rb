6a0000f0c61552084a73c24c272e99038c4eb7d0
adrien hucq <hucq.adrien@outlook.com>
2023-08-12 15:00:10 +0200

    Finished exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+  get 'reviews/show'
+  get 'reviews/new'
+  get 'reviews/create'
+  resources :restaurants, only: %i[index show new create] do
+    resources :reviews, only: %i[index show new create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
